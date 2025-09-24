b96d61498a961432ce25666af68e36521df255a8
Imam Bari Setiawan <imambarisetiawan695@gmail.com>
2025-09-25 05:01:33 +0700

    Update LoginRegis.py

--- LoginRegis.py
+++ LoginRegis.py
@@ -1,54 +1,35 @@
-import firebase_admin
-from firebase_admin import credentials, initialize_app, auth
 import streamlit as st
 from streamlit_option_menu import option_menu
 from login_register import login, signup
-# import homepage
-# import chatbot
-# import deteksi_penyakit
+from firebase_init import initialize_firebase
 
-# Function to initialize Firebase
-def initialize_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
-        firebase_admin.initialize_app(cred)
+# Init Firebase sekali di awal
+initialize_firebase()
 
-# Main function
 def main():
-    initialize_firebase()
-
-    # Check if user is logged in
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
 
-    # If not logged in, show login or signup page
     if not st.session_state['logged_in']:
-        login_or_signup = st.selectbox(
-            "Login/Sign Up",
-            ("Login", "Sign Up")
-        )
-
-        if login_or_signup == "Login":
+        choice = st.selectbox("Login / Sign Up", ("Login", "Sign Up"))
+        if choice == "Login":
             login()
         else:
             signup()
     else:
-        # Show content page with sidebar after login
         with st.sidebar:
             selected = option_menu(
                 menu_title="Main Menu",
                 options=["Home", "Deteksi Penyakit", "Chatbot"],
                 icons=["house", "file-medical", "chat"],
                 menu_icon="cast",
-                default_index=0,
+                default_index=0
             )
 
-        # Display the selected page
         if selected == "Home":
-            # homepage.app()
             st.write("Home")
         elif selected == "Deteksi Penyakit":
-            st.write("Deteksi penyakit")
+            st.write("Deteksi Penyakit")
         elif selected == "Chatbot":
             st.write("Chatbot")
 
